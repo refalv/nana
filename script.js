bf44934895240d24ab2de510516d63d43cc24d34
unknown <refalvi@yandex.com>
2025-02-06 16:40:27 +0700

    First commit

---
+++ script.js
@@ -0,0 +1,11 @@
+document.getElementById("yes").addEventListener("click", function () {
+    document.getElementById("message").innerText = "Yay! ❤️ I love you!";
+});
+
+document.getElementById("no").addEventListener("mouseover", function () {
+    let x = Math.random() * window.innerWidth - 100;
+    let y = Math.random() * window.innerHeight - 50;
+    this.style.position = "absolute";
+    this.style.left = `${x}px`;
+    this.style.top = `${y}px`;
+});
